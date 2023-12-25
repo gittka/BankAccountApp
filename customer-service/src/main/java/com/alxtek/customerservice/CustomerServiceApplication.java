ab1b3c8f01fd91f56f28ed08782dcb17793272ce
TK@ <alextchoupou@gmail.com>
2023-12-25 04:59:54 +0100

    Dockerize all the microservices

--- customer-service/src/main/java/com/alxtek/customerservice/CustomerServiceApplication.java
+++ customer-service/src/main/java/com/alxtek/customerservice/CustomerServiceApplication.java
@@ -1,13 +1,16 @@
 package com.alxtek.customerservice;
 
+import com.alxtek.customerservice.config.GlobalConfig;
 import com.alxtek.customerservice.entities.Customer;
 import com.alxtek.customerservice.repository.CustomerRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableConfigurationProperties(GlobalConfig.class)
 public class CustomerServiceApplication {
 
 	public static void main(String[] args) {
